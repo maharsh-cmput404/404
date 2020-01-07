27fd302807be86f29c619933baef31d090e8334c
Maharsh Patel <patel@maharsh.net>
2020-01-06 17:43:02 -0700

    download script code

--- labs/1/script.py
+++ labs/1/script.py
@@ -3,3 +3,7 @@ import requests
 print(requests.__version__)
 
 print(requests.get("http://google.com/").status_code)
+
+script_url = "https://raw.githubusercontent.com/maharshmellow/404/master/labs/1/script.py"
+
+print(requests.get(script_url).content)
